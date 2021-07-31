1e91afa60176e8bbd1b3f39dc7b45a2d727dd2cc
ArnaldoSantos <arnaldoufcg@gmail.com>
2021-07-31 14:26:17 -0300

    :tada: Criada a estrutura do projeto - Clean Archicteture

---
+++ BuildersApp/src/main/java/io/platformbuilders/BuildersAppApplication.java
@@ -0,0 +1,13 @@
+package io.platformbuilders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildersAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildersAppApplication.class, args);
+	}
+
+}
---
+++ BuildersApp/src/test/java/io/platformbuilders/BuildersAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.platformbuilders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildersAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
